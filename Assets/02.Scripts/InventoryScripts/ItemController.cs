9d7e2ee551fc484736ba7c9d76f218b89c5a4f84
RealStone <realstone2018@gmail.com>
2019-02-18 16:15:53 +0900

    JsonUtility Test

--- Assets/02.Scripts/InventoryScripts/ItemController.cs
+++ Assets/02.Scripts/InventoryScripts/ItemController.cs
@@ -1,8 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
+[Serializable]
+public class Stats
+{
+    public string stat;
+    public int value;
+}
+
+[Serializable]
+public class ItemData
+{
+    public string name;
+    public string sort;
+    public string description;
+    //public List<Stats> stats;
+    public Stats[] stats;
+}
 
 public class ItemController : MonoBehaviour {
+
+    public Stats CreateStats(string _stat, int _value)
+    {
+        Stats stats = new Stats();
+        stats.stat = _stat;
+        stats.value = _value;
+        return stats;
+    }
+
+    private void Start()
+    {
+        //ItemData[] itemData = new ItemData [2];
+        ItemData itemData = new ItemData();
+
+        List<Stats> statList = new List<Stats>();
+        statList.Add(CreateStats("Power", 3));
+        statList.Add(CreateStats("Speed", 1));
+
+
+        itemData.name = "AA";
+        itemData.sort = "Equip";
+        itemData.description = "This is Helmet";
+        itemData.stats = statList.ToArray();
+
+        string toJson = JsonUtility.ToJson(itemData);
+
+        File.WriteAllText(Application.dataPath + "/Saves/data.json", toJson);
+
+        Debug.Log(toJson);
+    }
+
 
 }
